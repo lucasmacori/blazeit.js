703bea0a0dd9b317d0cdae98370446e04a0c1a03
LegitBaboulinet <inscriptions@lucasmacori.fr>
2019-03-02 17:12:52 +0100

    Fixed very gross bug when not giving database information

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -2,10 +2,6 @@ import {Blazeit} from '../index';
 
 const blazeit = new Blazeit(
     {
-        database: {
-            hostname: 'localhost',
-            name: 'test_blazeit',
-        },
         models: [
             {
                 person: {
